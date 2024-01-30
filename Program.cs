c1ddbecbfa37a6e09152d879f29ed4119ec62de5
Mobeen Butt <144439858+MobeenButt@users.noreply.github.com>
2024-01-30 23:53:32 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Self2
+namespace Constructor
 {
-   
     internal class Program
     {
         static void Main(string[] args)
         {
-            Class2 n1 = new Class2("Mobeen", 10, 20, 40, 90);
-            Class2 n2 = new Class2("butt", 130, 220, 540, 690);
-            Class2 n3 = new Class2("3loop", 320, 120, 3340, 3290);
-            Class2[] n = new Class2[3];
-            n[0] = n1;
-            n[1] = n2;
-            n[2] = n3;
-
-
-            for (int i = 0; i < n.Length; i++)
-            {
-                Console.WriteLine(n[i].sname + " " + n[i].matricMarks);
-            }
-
-            Console.ReadLine();
+            student s1 =new student();
+            s1.sname = "Jack";
+            student s2 =new student();
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s2.sname);
+            Console.ReadKey();
         }
     }
 }
