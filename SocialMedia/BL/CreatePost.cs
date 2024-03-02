0e87f7b071f96a7fcfe34194ad9af961ed340c54
Mobeen Butt <144439858+MobeenButt@users.noreply.github.com>
2024-03-02 23:59:34 +0500

    Add files via upload

---
+++ SocialMedia/BL/CreatePost.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.BL
+{
+    internal class CreatePost
+    {
+        private List<string> comments = new List<string>();
+        private int like;
+
+        public CreatePost()
+        {
+        }
+
+        public CreatePost(List<string> comments, int like)
+        {
+            this.comments = comments;
+            this.like = like;
+        }
+
+        public int GetLike()
+        {
+            return like;
+        }
+
+        public string GetComment(string comment)
+        {
+            string com = "";
+            foreach (string c in comments)
+            {
+                com = com + c;
+            }
+            return com;
+        }
+
+        public void SetComment(string comment)
+        {
+            if (comments != null)
+                comments.Add(comment);
+        }
+
+        public void SetLike(int like)
+        {
+            this.like = like;
+        }
+    }
+}
---
+++ SocialMedia/Program.cs
@@ -0,0 +1,34 @@
+﻿using SocialMedia.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   Follower Follower1 = new Follower("Mobeen");
+       
+            CreatePost Post1 = new CreatePost();
+            Post1.SetLike(10000);
+            Post1.SetComment("Ok");
+
+            Follower1.AddPost(Post1);
+
+            Console.WriteLine("Follower of Follower1");
+            Follower1.ViewFollowing();
+
+            Console.WriteLine("Posts of Follower1");
+            Follower1.ViewPost();
+
+            Console.WriteLine("Followers of Follower1");
+            Follower1.ViewFollower();
+
+            Console.ReadLine();
+        }
+    }
+}
+  
